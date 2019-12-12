ef8ecf5116c651b7a6a1f1ae7c362807c5a652c8
your username <vitor13m2003@gmail.com>
2019-12-11 21:48:57 -0300

    email sending is working

--- api/dist/main.js
+++ api/dist/main.js
@@ -9,9 +9,10 @@ const service_router_1 = require("./source/routes/service.router");
 const payment_router_1 = require("./source/routes/payment.router");
 const schedule_router_1 = require("./source/routes/schedule.router");
 const attendance_router_1 = require("./source/routes/attendance.router");
+const mail_router_1 = require("./source/mail/mail.router");
 const enviroment_1 = require("./source/config/enviroment");
 const server = new server_1.Server();
-server.bootstrap([client_router_1.clientRouter, employee_router_1.employeeRouter, address_router_1.addressRouter, position_router_1.positionRouter, service_router_1.serviceRouter, payment_router_1.paymentRouter, schedule_router_1.scheduleRouter, attendance_router_1.attendanceRouter]).then(server => {
+server.bootstrap([client_router_1.clientRouter, employee_router_1.employeeRouter, address_router_1.addressRouter, position_router_1.positionRouter, service_router_1.serviceRouter, payment_router_1.paymentRouter, schedule_router_1.scheduleRouter, attendance_router_1.attendanceRouter, mail_router_1.mailRouter]).then(server => {
     console.log(`Server is listening on ${enviroment_1.enviroment.server.port}`);
 }).catch(error => {
     console.log('Server failed to start');
--- api/main.ts
+++ api/main.ts
@@ -7,13 +7,11 @@ import { serviceRouter } from './source/routes/service.router';
 import { paymentRouter } from './source/routes/payment.router';
 import { scheduleRouter } from './source/routes/schedule.router';
 import { attendanceRouter } from './source/routes/attendance.router';
+import { mailRouter } from './source/mail/mail.router';
 import { enviroment } from './source/config/enviroment';
 
-
-
-
 const server = new Server();
-server.bootstrap([clientRouter, employeeRouter, addressRouter, positionRouter, serviceRouter, paymentRouter, scheduleRouter, attendanceRouter]).then(server => {
+server.bootstrap([clientRouter, employeeRouter, addressRouter, positionRouter, serviceRouter, paymentRouter, scheduleRouter, attendanceRouter, mailRouter]).then(server => {
   console.log(`Server is listening on ${enviroment.server.port}`)
 }).catch(error => {
   console.log('Server failed to start');
